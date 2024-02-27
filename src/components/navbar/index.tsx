a5c5ecadb66ab71c59fd41effcbdf7264ab5ed70
cephaske254 <cephaske254@gmail.com>
2024-02-27 11:06:10 +0300

    update navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,6 +7,10 @@ import Container from "@mui/material/Container";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import MenuList from "@mui/material/MenuList";
+import Divider from "@mui/material/Divider";
 
 import Search from "@mui/icons-material/Search";
 import CloseOutlined from "@mui/icons-material/CloseOutlined";
@@ -15,6 +19,7 @@ import { NAVBAR_HEIGHT } from "./constants";
 import Logo from "../logo";
 import { SearchContext } from "@/contexts/search-context";
 import NavbarSearch from "./search";
+import useResponsive from "@/hooks/useResponsive";
 
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
   top: 0,
@@ -25,20 +30,83 @@ const StyledAppBar = styled(AppBar)(({ theme }) => ({
   borderBottom: `1px solid ${theme.palette.divider}`,
 }));
 
-const RootContainer = styled(Container)({
+const RootContainer = styled(Container)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
   height: "100%",
-});
+
+  ".MuiMenuItem-root": {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+
+    "&.primary": {
+      color: theme.palette.primary.main,
+      display: "flex",
+      alignItems: "center",
+    },
+  },
+}));
 
 const Navbar = () => {
+  const isMobile = useResponsive("down", "md");
   return (
     <StyledAppBar position="fixed" elevation={0} color="transparent">
       <RootContainer>
         <Stack direction="row" flex={1} spacing={4} alignItems="center">
           <Logo />
-          <NavbarSearch />
+
+          {!isMobile && (
+            <>
+              <MenuItem disableRipple disableTouchRipple className="primary">
+                <img src="/icon-category.svg" />
+                <Typography ml={1} variant="body2">
+                  카테고리
+                </Typography>
+              </MenuItem>
+
+              <NavbarSearch />
+
+              <MenuList sx={{ display: "flex" }} disableListWrap>
+                <MenuItem
+                  disableRipple
+                  disableTouchRipple
+                  disableGutters
+                  sx={{ px: 1.5 }}
+                >
+                  <img src="/home-event.svg" />
+                </MenuItem>
+                <Divider flexItem orientation="vertical" variant="middle" />
+                <MenuItem
+                  disableRipple
+                  disableTouchRipple
+                  disableGutters
+                  sx={{ px: 1 }}
+                >
+                  <Typography ml={0.5} variant="caption" color="grey.900">
+                    로그인 / 회원가입
+                  </Typography>
+                </MenuItem>
+              </MenuList>
+            </>
+          )}
+
+          {isMobile && (
+            <Stack
+              flexGrow={1}
+              justifyContent="flex-end"
+              direction="row"
+              spacing={2}
+            >
+              <IconButton>
+                <img src="/bell.svg" height={20} width={20} />
+              </IconButton>
+              <IconButton>
+                <img src="/search.svg" height={20} width={20} />
+              </IconButton>
+            </Stack>
+          )}
         </Stack>
       </RootContainer>
     </StyledAppBar>
